4eaa24be811eac57873f81ec9a3e8f67b30265ad
chamaxx9zp <chamakapiyumal@gmail.com>
2025-01-09 20:10:28 +0530

    arrow function breakdown  ↔

--- index.js
+++ index.js
@@ -2,6 +2,19 @@ import express from 'express';
 
 const app = express();
 
+function getHandler(){
+    console.log("get handler function running")
+} 
+
+
+app.get("/",getHandler
+)
+
+app.post("/",
+    ()=>{
+        console.log("Hello World Post Request")
+    }
+)
 
 
 app.listen(
