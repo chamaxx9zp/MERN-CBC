7b018aa93f4eb1d845f0ce3847ca875b94ab0a71
chamaxx9zp <chamakapiyumal@gmail.com>
2025-01-20 00:05:24 +0530

    add user login login 🌟

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,8 +1,13 @@
 import User from "../Models/user.js";
+import bcrypt from "bcrypt";
 
 export function createUser(req,res){
-    const user = new User(req.body)
-    user.save().then(()=>{
+
+    const newUserdata = req.body
+    newUserdata.password = bcrypt.hashSync(newUserdata.password, 10)
+
+    const newUser = new User(newUserdata)
+    newUser.save().then(()=>{
         res.json({
             message:"User is created"
         })
@@ -20,4 +25,29 @@ export function getUsers(req,res){
              list:userList
          })
     })
+}
+
+export function loginUser(req,res){
+    User.find({email:req.body.email}).then(
+        (users)=>{
+            if(users.length == 0){
+                res.json({
+                    message:"User not found"
+                })
+            }else{
+                const user = users[0]
+                const isPasswordCorrect = bcrypt.compareSync(req.body.password,user.password)
+
+                if (isPasswordCorrect){
+                    res.json({
+                        message: "User Logged In"
+                    })
+                }else{
+                    res.json({
+                        message:"User Not Logged In. Wrong Password"
+                    })
+                }
+            }
+        }
+    )
 }
--- routes/userRouter.js
+++ routes/userRouter.js
@@ -1,11 +1,12 @@
 import express from 'express';
-import { createUser, getUsers } from '../controllers/userController.js';
+import { createUser, getUsers, loginUser } from '../controllers/userController.js';
 
 // create student router
 const userRouter = express.Router();
 
 userRouter.get("/",getUsers)
 userRouter.post("/",createUser)
+userRouter.post("/login",loginUser)
 
 // studentRouter.delete("/",deleteStudent)
 
