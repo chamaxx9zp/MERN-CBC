d71ff97d3052a162a82489db948b4c2dfebbe1dc
chamaxx9zp <chamakapiyumal@gmail.com>
2025-01-23 14:16:24 +0530

    remove unnecessary code 🧹🙂

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
-import studentRouter from './routes/studentRouter.js';
-import productRouter from './routes/productRouter.js';
 import userRouter from './routes/userRouter.js';
 import jwt from 'jsonwebtoken';
 import dotenv from "dotenv";
@@ -36,38 +34,8 @@ app.use(
         next()
     })
 
-app.use("/students",studentRouter)
-app.use("/products",productRouter)
 app.use("/users",userRouter)
 
-// app.get("/",
-//     (req,res)=>{
-//         console.log(req.body)
-//         console.log("Hello World this is get request")
-
-//         res.json({
-//                 message: "Hello World Response"
-//             })
-//     }
-// )
-
-// app.post("/",
-//     (req,res)=>{
-//         // console.log(req.body);
-//         // console.log("Hello World Post Request")
-//         // res.json({
-//         //         message:"Response from the Post" + req.body.name
-//         //     })
-//         const newStudent = new Student(req.body)
-//         newStudent.save().then(
-//             ()=>{
-//                 res.json({
-//                     message:"student created"
-//                 })
-//             }
-//         )
-//     }
-// )
 
 app.listen(
     3001,
