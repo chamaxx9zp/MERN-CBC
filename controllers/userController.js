0231135c6a778eaff50c65ff07c03b91485ec22a
chamaxx9zp <chamakapiyumal@gmail.com>
2025-01-23 16:46:56 +0530

    create validation fumction function for the admin 🔒✔️

--- controllers/userController.js
+++ controllers/userController.js
@@ -82,3 +82,15 @@ export function loginUser(req,res){
     )
 }
 
+export function isAdmin(req){
+    if(req.user == null){
+        return false
+    }
+
+    if(req.user.type != "admin"){
+        return false
+    }
+
+    return true
+}
+
